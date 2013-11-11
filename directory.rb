2bb94eeccf1d14ba111273f23ce33da222b2041a
Gianni Guitteaud <gianni.guitteaud@gmail.com>
2013-11-11 16:54:40 +0000

    Added Directory Function

--- directory.rb
+++ directory.rb
@@ -1,11 +1,43 @@
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-puts "Tiger Woods"
-puts "Darth Vador"
-puts "Charly Sheen"
-puts "Jay Z"
-puts "Ryan Leslie"
-puts "Rick Ross"
-puts "John Mayer"
-puts "Ed Sheeran"
-puts "Nina Simone"
+
+def input_students 
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+	students = []
+	# get the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		students << { :name => name, :cohort => :november}
+		puts "Now we have #{students.length} students"
+		#get another name from the user
+		name = gets.chomp
+	end
+	#return the array of students
+	students
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]})"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.length} great students"
+end
+
+# nothing happens until we call the methods
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
+
